4d981e35df774b6fb336c759a44a7872f214b838
Gavin <gamichwi@gmail.com/>
2022-11-17 22:23:48 +1100

    Added webpack module

---
+++ 04-webpack/exercise/src/script.js
@@ -0,0 +1,32 @@
+import './style.css';
+import * as THREE from 'three';
+
+// Scene
+const scene = new THREE.Scene();
+
+//Red cube
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const redCube = new THREE.Mesh(geometry, material);
+scene.add(redCube);
+
+//Sizes
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
+//Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+camera.position.x = 2;
+camera.position.y = 1;
+scene.add(camera);
+
+//Renderer
+const canvas = document.querySelector('.webgl');
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas
+});
+renderer.setSize(sizes.width, sizes.height)
+renderer.render(scene, camera);
