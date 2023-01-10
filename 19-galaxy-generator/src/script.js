743b1d2b79e9349a96e57821c0b2864ea34b4251
Gavin <gamichwi@gmail.com/>
2023-01-10 11:53:30 +1100

    full screen on double click

--- 19-galaxy-generator/src/script.js
+++ 19-galaxy-generator/src/script.js
@@ -249,4 +249,24 @@ const tick = () => {
   window.requestAnimationFrame(tick);
 };
 
+// Full screen upon double click
+window.addEventListener('dblclick', () => {
+  const fullscreenElement =
+    document.fullscreenElement || document.webkitFullscreenElement;
+
+  if (!fullscreenElement) {
+    if (canvas.requestFullscreen) {
+      canvas.requestFullscreen();
+    } else if (canvas.webkitRequestFullscreen) {
+      canvas.webkitRequestFullscreen;
+    }
+  } else {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) {
+      document.webkitExitFullscreen;
+    }
+  }
+});
+
 tick();
