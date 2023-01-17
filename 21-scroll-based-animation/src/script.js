6476ca59be7d0e396d74e5e8f311d979229d7ffb
Gavin <gamichwi@gmail.com/>
2023-01-17 18:49:48 +1100

    added double click to full screen

--- 21-scroll-based-animation/src/script.js
+++ 21-scroll-based-animation/src/script.js
@@ -256,3 +256,23 @@ const tick = () => {
 };
 
 tick();
+
+// Full screen upon double click
+window.addEventListener('dblclick', () => {
+    const fullscreenElement =
+      document.fullscreenElement || document.webkitFullscreenElement;
+  
+    if (!fullscreenElement) {
+      if (canvas.requestFullscreen) {
+        canvas.requestFullscreen();
+      } else if (canvas.webkitRequestFullscreen) {
+        canvas.webkitRequestFullscreen;
+      }
+    } else {
+      if (document.exitFullscreen) {
+        document.exitFullscreen();
+      } else if (document.webkitExitFullscreen) {
+        document.webkitExitFullscreen;
+      }
+    }
+  });
